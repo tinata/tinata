40cfd999c4db6d3bf054852069b06eefce705451
Iain Collins <me@iaincollins.com>
2014-01-18 23:28:11 +0000

    API now includes historical crime data and flags warnings if area is a hot spot for certain types of crime.

--- scripts/import.js
+++ scripts/import.js
@@ -68,6 +68,82 @@ init()
     csvConverter.from("../data/LGBT.csv");
     return deferred.promise;
 })
+.then(function(countries) {
+
+    // Loop through all countries (defined by UN and add 3 letter ISO abbr,
+    // use the FCO name and note if it's in the FCO DB or not.
+    var deferred = Q.defer();
+    var csvConverter = new Converter();
+    csvConverter.on("end_parsed", function(jsonObj) {
+        var britsAbroad = jsonObj.csvRows;
+        for (i in countries) {
+            countries[i].inFCODB = "false";
+            for (j in britsAbroad) {
+                if (countries[i].iso == britsAbroad[j]['The two-letter ISO 3166-1 code']) {
+                    
+                    countries[i].uk = {};
+
+                    if (parseInt(britsAbroad[j]['Drug Arrests']) > 0)
+                        countries[i].uk.drugArrests = parseInt(britsAbroad[j]['Drug Arrests']);
+
+                    if (parseInt(britsAbroad[j]['Total Arrests / Detentions']) > 0)
+                            countries[i].uk.arrests = parseInt(britsAbroad[j]['Total Arrests / Detentions']);
+
+                    if (parseInt(britsAbroad[j]['Total Deaths']) > 0)
+                        countries[i].uk.deaths = parseInt(britsAbroad[j]['Total Deaths']);
+
+                    if (parseInt(britsAbroad[j]['Hospitalisation']) > 0)
+                        countries[i].uk.hospitalizations = parseInt(britsAbroad[j]['Hospitalisation']);
+
+                    if (parseInt(britsAbroad[j]['Rape']) > 0)
+                        countries[i].uk.rapes = parseInt(britsAbroad[j]['Rape']);
+
+                    if (parseInt(britsAbroad[j]['Sexual Assault']) > 0)
+                        countries[i].uk.sexualAssaults = parseInt(britsAbroad[j]['Sexual Assault']);
+
+                    if (parseInt(britsAbroad[j]['Total Other Assistance']) > 0)
+                        countries[i].uk.givenOtherConsularAssistance = parseInt(britsAbroad[j]['Total Other Assistance']);
+
+                    if (parseInt(britsAbroad[j]['Passport Lost/Stolen']) > 0)
+                        countries[i].uk.lostPassport = parseInt(britsAbroad[j]['Passport Lost/Stolen']);
+
+                    if (parseInt(britsAbroad[j]['IPS Visitors']) > 0)
+                        countries[i].uk.visitors = parseInt(britsAbroad[j]['IPS Visitors']);
+
+
+                    // Add warnings if level of activity is above normal
+                    countries[i].warnings = {};
+                    if (britsAbroad[j]['Drug Arrests'].indexOf('HIGH') > 0)
+                        countries[i].warnings.drugArrests = 'High';
+                    
+                    if (britsAbroad[j]['Total Arrests / Detentions'].indexOf('HIGH') > 0)
+                        countries[i].warnings.arrests = 'High';
+                    
+                    if (britsAbroad[j]['Total Deaths'].indexOf('HIGH') > 0)
+                        countries[i].warnings.deaths = 'High';
+                    
+                    if (britsAbroad[j]['Hospitalisation'].indexOf('HIGH') > 0)
+                        countries[i].warnings.hospitalizations = 'High';
+                    
+                    if (britsAbroad[j]['Rape'].indexOf('HIGH') > 0)
+                        countries[i].warnings.rapes = 'High';
+                    
+                    if (britsAbroad[j]['Sexual Assault'].indexOf('HIGH') > 0)
+                        countries[i].warnings.sexualAssaults = 'High';
+                    
+                    if (britsAbroad[j]['Total Other Assistance'].indexOf('HIGH') > 0)
+                        countries[i].warnings.givenOtherConsularAssistance = 'High';
+                    
+                    if (britsAbroad[j]['Passport Lost/Stolen'].indexOf('HIGH') > 0)
+                        countries[i].warnings.lostPassport = 'High';
+                }
+            }
+        }
+        deferred.resolve(countries);
+    });
+    csvConverter.from("../data/British-Behaviour-Abroad_2012-2013.csv");
+    return deferred.promise;
+})
 // @todo Import FCO travel advice
 // @todo Import Human rights info
 // @todo Import Relevant other data (e.g. currency)
@@ -85,7 +161,7 @@ init()
     return Q.all(promises);
 })
 .then(function(countries) {
-   console.log(countries);
+    console.log(countries);
     console.log("*** Finished importing data into the DB");
     db.close();
 });
